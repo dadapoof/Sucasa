f7c47723f0ec540393cd60dc7deee3b7013bdf4c
David Massart <32443128+dadapoof@users.noreply.github.com>
2018-12-01 16:13:12 -0500

    header

--- client/src/Components/HeaderComponents/NavBar.jsx
+++ client/src/Components/HeaderComponents/NavBar.jsx
@@ -11,6 +11,7 @@ class NavBar extends Component {
     return (
     <div>
       <header>
+      <div class="test">
         <ul id="headerButtons">
           <li className="LogoButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="subnav_link">SC</Link></li>
           <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="subnav_link">Home</Link></li>
@@ -20,6 +21,7 @@ class NavBar extends Component {
           <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="subnav_link">Gallery</Link></li>
           <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="subnav_link">Contact</Link></li>
         </ul>
+        </div>
       </header>
       <h1 class="centered-text-image">SuCasa</h1>
     </div>
