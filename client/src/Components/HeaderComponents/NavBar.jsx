aa354e872f55186dcc27e10f4dc3d38c100d33a3
David Massart <32443128+dadapoof@users.noreply.github.com>
2018-11-30 21:30:30 -0500

    ok...

--- client/src/Components/HeaderComponents/NavBar.jsx
+++ client/src/Components/HeaderComponents/NavBar.jsx
@@ -14,8 +14,8 @@ class NavBar extends Component {
           <li className="navButton"><Link to="">Home</Link></li>
         </ul>
       </header>
-        const elem = <h1>Hello World</h1>;
-    	const el = <h2>This is Sucasa</h2>;
+        <h1>Hello World</h1>
+    	<h2>This is Sucasa</h2>
     )
   }
 }
