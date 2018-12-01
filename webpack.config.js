e04596bc02fb34f636b16a59fe9fc12e78452ec7
David Massart <32443128+dadapoof@users.noreply.github.com>
2018-11-30 22:13:58 -0500

    file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -29,10 +29,8 @@ module.exports = {
         test: /\.png$/,
         loader: 'url-loader?limit=100000&minetype=image/png'
       },
-      {
-        test: /\.jpg/,
-        loader: 'file-loader'
-      },
+      {test: /\.(png|jpg)$/, loader: 'file-loader'},
+      {test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, loader: 'file-loader'},
       {
         test : /\.jsx?/,
         include : SRC_DIR,
