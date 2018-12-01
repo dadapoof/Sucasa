dbafd64468e29265d6f008fe690b6496fe075496
David Massart <32443128+dadapoof@users.noreply.github.com>
2018-12-01 12:20:30 -0500

    image CSS

--- client/src/Components/HeaderComponents/NavBar.jsx
+++ client/src/Components/HeaderComponents/NavBar.jsx
@@ -14,7 +14,7 @@ class NavBar extends Component {
         <ul id="headerButtons">
           <li className="navButton"><Link to="">Home</Link></li>
         </ul>
-        <img src={require('./apartment.jpg')} />
+        <img id="nav-bar-image"src={require('./apartment.jpg')} />
       </header>
     </div>
     );
