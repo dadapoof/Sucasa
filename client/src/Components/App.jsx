22774468a03b1721d5f43384ea05b57c8ae22909
David Massart <32443128+dadapoof@users.noreply.github.com>
2018-11-30 21:38:07 -0500

    Router stuff

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -15,6 +15,21 @@ class App extends Component {
       <Router>
         <div>
           <NavBar />
+	        <ul>
+	          <li>
+	            <Link to="/">HomePage</Link>
+	          </li>
+	          <li>
+	            <Link to="/HeaderComponents/NavBar.jsx">NavBar</Link>
+	          </li>
+	          <li>
+	            <Link to="/FooterComponents/Footer.jsx">Footer</Link>
+	          </li>
+	        </ul>
+
+	        <hr />
+
+
           <Route name="home" exact path="/" component={HomePage} />
           <Route name="nav" exact path="/HeaderComponents/NavBar.jsx" component={NavBar} />
           <Route name="footer" exact path="/FooterComponents/Footer.jsx" component={Footer} />
