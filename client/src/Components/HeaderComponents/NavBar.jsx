366939d1440294d81eddb4e4bffba28ab9d61890
David Massart <32443128+dadapoof@users.noreply.github.com>
2018-12-01 20:49:59 -0500

    animation

--- client/src/Components/HeaderComponents/NavBar.jsx
+++ client/src/Components/HeaderComponents/NavBar.jsx
@@ -8,12 +8,15 @@ import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
   render() {
+  	function hover(e) {
+	    e.setState({color: 'rgba(0, 0, 0, 0.6)'});
+  	}
     return (
     <div>
       <header>
         <div class="test">
           <ul id="headerButtons">
-          <li className="LogoButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="subnav_link">SC</Link></li>
+          <li className="LogoButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} onClick=(hover) className="subnav_link">SC</Link></li>
           <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="subnav_link">Home</Link></li>
           <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="subnav_link">What to Expect</Link></li>
           <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="subnav_link">Amenities</Link></li>
