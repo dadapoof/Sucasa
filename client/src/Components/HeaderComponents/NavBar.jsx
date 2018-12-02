a01408ae4a364e579be09c50dd5d19a6eca870e0
David Massart <32443128+dadapoof@users.noreply.github.com>
2018-12-01 20:58:24 -0500

    navbar

--- client/src/Components/HeaderComponents/NavBar.jsx
+++ client/src/Components/HeaderComponents/NavBar.jsx
@@ -8,21 +8,18 @@ import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
   render() {
-  	function hover(e) {
-	    e.setState({color: 'rgba(0, 0, 0, 0.6)'});
-  	}
     return (
     <div>
       <header>
         <div class="test">
           <ul id="headerButtons">
-          <li className="LogoButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} onClick={this.hover} className="subnav_link">SC</Link></li>
-          <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="subnav_link">Home</Link></li>
-          <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="subnav_link">What to Expect</Link></li>
-          <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="subnav_link">Amenities</Link></li>
-          <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="subnav_link">Around the Area</Link></li>
-          <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="subnav_link">Gallery</Link></li>
-          <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="subnav_link">Contact</Link></li>
+          <li className="LogoButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }}>SC</Link></li>
+          <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="link">Home</Link></li>
+          <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="link">What to Expect</Link></li>
+          <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="link">Amenities</Link></li>
+          <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="link">Around the Area</Link></li>
+          <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="link">Gallery</Link></li>
+          <li className="navButton"><Link to="" style={{ textDecoration: 'none', color: 'white' }} className="link">Contact</Link></li>
         </ul>
        </div>
       </header>
